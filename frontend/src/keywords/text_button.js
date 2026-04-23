ec932b8865138a94499883fc82a87cbcb7d53a18
simon.edvin <simwick03@gmail.com>
2026-04-23 14:47:35 +0200

    Added hover trigger

--- frontend/src/keywords/text_button.js
+++ frontend/src/keywords/text_button.js
@@ -1,5 +1,7 @@
- import triggers from "./triggers.js"
- 
+ import triggers from "./triggers.js";
+ // import { useRef } from "react";
+
+
   //hitta triggers
   function trigger(word) {
     return triggers.find(trigger => trigger.keyword === word.toLowerCase().replace(/[.,!?;:()"']/g, ""));
@@ -13,23 +15,48 @@
 
 
   function renderStoryText(text) {
+    // const lastTriggered = useRef(null); // avoid re-triggering same word
+
+    function handlePointerMove(e) {
+      const element = document.elementFromPoint(e.clientX, e.clientY); // get element where finger is
+
+      if (element?.dataset?.trigger) { // && element.dataset.trigger !== lastTriggered.current maybe
+      //lastTriggered.current = element.dataset.trigger; maybe add later
+      play(element.dataset.trigger);
+    }
+    }
+
+    /*function handlePointerEnd() {
+      lastTriggered.current = null; // reset when finger lifts
+    }*/
+
+
     const words = text.split(/\s+/); //split on space
 
-    return words.map((word) => {
-      const triggers = trigger(word);
-
-      if (triggers) {
-        return (
-          <button
-            onClick={() => play(triggers.sound)}
-            className="word-button">
-            {word}{" "}
-          </button>
-        );
-      }
-
-      return <span>{word} </span>;
-    });
-  }
+    return (
+    <div
+      onPointerMove={handlePointerMove}
+      //onPointerUp={handlePointerEnd}
+      //onPointerLeave={handlePointerEnd}
+      style={{ touchAction: "none" }} // prevents scroll interfering
+    >
+      {words.map((word, i) => {
+        const triggers = trigger(word);
+        if (triggers) {
+          return (
+            <span
+              key={i}
+              data-trigger={triggers.sound} // store sound path in DOM
+              className="word-button"
+            >
+              {word}{" "}
+            </span>
+          );
+        }
+        return <span key={i}>{word} </span>;
+      })}
+    </div>
+  );
+}
 
   export default renderStoryText;
