9083c4df15d6c5e48ed2113f9353ad0acb7e9ea4
simon.edvin <simwick03@gmail.com>
2026-04-29 13:47:51 +0200

    Tried adding onTouch for iOS

--- frontend/src/keywords/text_button.js
+++ frontend/src/keywords/text_button.js
@@ -2,30 +2,46 @@
  // import { useRef } from "react";
 
 
+let audioUnlocked = false;
+
+function unlockAudio() {
+  if (audioUnlocked) return;
+  const silence = new Audio();
+  silence.play().then(() => {
+    audioUnlocked = true;
+  }).catch(() => {});
+}
+
   //hitta triggers
-  function trigger(word) {
-    return triggers.find(trigger => trigger.keyword === word.toLowerCase().replace(/[.,!?;:()"']/g, ""));
-  }
+function trigger(word) {
+  return triggers.find(trigger => trigger.keyword === word.toLowerCase().replace(/[.,!?;:()"']/g, ""));
+}
 
   //spela upp ljudeffekt 
-  function play(soundFile) {
-    const audio = new Audio(soundFile);
-    audio.play();
-  }
+function play(soundFile) {
+  const audio = new Audio(soundFile);
+  audio.play();
+}
 
 
   function renderStoryText(text) {
     // const lastTriggered = useRef(null); // avoid re-triggering same word
-
-    function handlePointerMove(e) {
-      const element = document.elementFromPoint(e.clientX, e.clientY); // get element where finger is
-
+    
+    function handleMove(e) {
+      let element
+      if (e.touches && e.touches.length > 0) {
+        element = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+      }
+      else {
+        element =  document.elementFromPoint(e.clientX, e.clientY);
+      }
+      
       if (element?.dataset?.trigger) { // && element.dataset.trigger !== lastTriggered.current maybe
       //lastTriggered.current = element.dataset.trigger; maybe add later
       play(element.dataset.trigger);
     }
     }
-
+    
     /*function handlePointerEnd() {
       lastTriggered.current = null; // reset when finger lifts
     }*/
@@ -35,20 +51,16 @@
 
     return (
     <div
-      onPointerMove={handlePointerMove}
-      //onPointerUp={handlePointerEnd}
-      //onPointerLeave={handlePointerEnd}
+      onPointerMove={handleMove}
+      onTouchMove={handleMove}
+      onTouchStart={unlockAudio}
       style={{ touchAction: "none" }} // prevents scroll interfering
     >
       {words.map((word, i) => {
         const triggers = trigger(word);
         if (triggers) {
           return (
-            <span
-              key={i}
-              data-trigger={triggers.sound} // store sound path in DOM
-              className="word-button"
-            >
+            <span key={i} data-trigger={triggers.sound} className="word-button">
               {word}{" "}
             </span>
           );
